713bc856417cc83709db101ad7c138a10eb82ce7
Denzy7 <dennismyner7@gmail.com>
2022-02-14 18:27:46 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# mc.sh
+Run singleplayer minecraft from shell
+
+# install
+**Assumes you have working installation of Minecraft**
+
+Copy to:  
+ `$HOME/.minecraft`
+ 
+Run:  
+ `./mc.sh <username> <fullversion> `
+ 
+Example:  
+ `./mc.sh Denzy7 1.17.1`
---
+++ mc.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+if [ -z "$1" ] || [ -z "$2" ]
+then
+	echo "Usage: mc.sh <username> <fullversion>"
+	exit 1
+fi
+
+NAME="$1"
+MC="$2"
+
+MCASSETS="${MC%.*}"
+MCDIR="versions/$MC"
+MCJAR="$PWD/$MCDIR/$MC.jar"
+
+if [ -f $MCJAR ]
+then
+	echo "Using Minecraft $MCJAR"
+else
+	echo "Minecraft $MCJAR not found!"
+	exit 1
+fi
+
+MCNATIVE="$MCDIR/natives"
+LIBS=$(find $PWD/libraries -type f | tr '\n' ':')
+JVMOPT="-Xms512m -Xmx1g"
+java $JVMOPT -Djava.library.path=$MCNATIVE -cp "$LIBS:$MCJAR" net.minecraft.client.main.Main -accessToken null --version $MC --assetIndex ${MCASSETS}  --username $NAME
